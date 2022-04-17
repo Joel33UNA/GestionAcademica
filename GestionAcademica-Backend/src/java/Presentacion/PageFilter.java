cfcaf5418b7c0b6730d77be5bf4b106b63b6806c
JOEL ZAMORA CHAVES <joel.zamora.chaves@est.una.ac.cr>
2022-04-16 20:23:49 -0600

    DELETE

--- GestionAcademica-Backend/src/java/Presentacion/PageFilter.java
+++ GestionAcademica-Backend/src/java/Presentacion/PageFilter.java
@@ -19,7 +19,7 @@ public class PageFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response,FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         ((HttpServletResponse) response).addHeader("Access-Control-Allow-Origin", "*");
-        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST");
+        ((HttpServletResponse) response).addHeader("Access-Control-Allow-Methods", "GET, OPTIONS, HEAD, PUT, POST, DELETE");
         ((HttpServletResponse) response).addHeader("Access-Control-Allow-Headers", "*");
         HttpSession session = httpRequest.getSession(true);
         HttpServletResponse resp = (HttpServletResponse) response;
